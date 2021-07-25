ebc95572ee383ed10c43233425ba35c99bb0365b
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-25 06:23:48 +0500

    Initial Commit

---
+++ CofeeOrderingApp/CofeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CofeeOrderingAppApp.swift
+//  CofeeOrderingApp
+//
+//  Created by sherry on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CofeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
