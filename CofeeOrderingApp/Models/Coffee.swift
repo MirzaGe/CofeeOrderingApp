9097b22394d729622eb80d956536f1008490a5f6
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-26 01:06:46 +0500

    commit

--- CofeeOrderingApp/Models/Coffee.swift
+++ CofeeOrderingApp/Models/Coffee.swift
@@ -7,4 +7,18 @@
 
 import Foundation
 
+struct Coffee {
+    let name: String
+    let  imageURL: String
+    let price: Double
+}
 
+extension Coffee {
+    static func all() -> [Coffee] {
+        return [
+            Coffee(name: "Cappuccino", imageURL: "Cappuccino", price: 2.5),
+            Coffee(name: "Expresso", imageURL: "Expresso", price: 2.1),
+            Coffee(name: "Regular", imageURL: "Regular", price: 1.0)
+        ]
+    }
+}
---
+++ CofeeOrderingApp/Models/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CofeeOrderingApp
+//
+//  Created by sherry on 25/07/2021.
+//
+
+import Foundation
